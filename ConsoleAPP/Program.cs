02e746d4e6872b6bea1a738b7d05ad0831f9e9b4
Txai de Sousa Mattos <txai.mattos@ambevtech.com.br>
2021-12-16 22:15:12 -0300

    initial

---
+++ ConsoleAPP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleAPP
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            RunFactoryMethodSample();
+        }
+
+        private static void RunFactoryMethodSample()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
